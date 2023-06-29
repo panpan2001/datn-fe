b0c90dc15884bfe4d76e5dbcb41a4f8c217dae36
panpan2001 <truonghoc19102001@gmail.com>
2023-06-30 02:11:14 +0700

    student report course

--- src/pages/AccountManagementPage/index.jsx
+++ src/pages/AccountManagementPage/index.jsx
@@ -16,12 +16,13 @@ import toLowerCaseNonAccentVietnamese from '../../contexts/toLowerCaseNonAccentV
 import { contextProvider } from '../../layouts/ParentLayouts/AdminManagementLayout'
 import { getAllStudentsSuccess } from '../../redux/slices/Student/getAllStudentSlice'
 import updateAccountStatus from '../../redux/actions/Account/UpdateAccountStatus'
+import { toast } from 'react-toastify'
 
 
 
 function AccountManagementPage() {
-  const searchValue= useContext(contextProvider)
-  console.log("search in account management",searchValue)
+  const searchValue = useContext(contextProvider)
+  console.log("search in account management", searchValue)
 
   const user = useSelector((state) => state.login.login?.currentUser)
   const dispatch = useDispatch()
@@ -44,36 +45,44 @@ function AccountManagementPage() {
     else if (role == "teacher") return "#C2E7FF"
     else return "#B2FFDA"
   }
-// console.log({accounts})
+  // console.log({accounts})
   const handleSearch = (e) => {
-    return searchValue== 0 ? e : 
-    (toLowerCaseNonAccentVietnamese(e.full_name)
-    .includes(toLowerCaseNonAccentVietnamese(searchValue))?
-    e :
-    (toLowerCaseNonAccentVietnamese(e.email)
-    .includes(toLowerCaseNonAccentVietnamese(searchValue)))?
-    e :
-    (moment(e.date_of_birth).format('DD/MM/YYYY').toString().includes(searchValue) ? e :
-    moment(e.createdAt).format('DD/MM/YYYY').toString().includes(searchValue) ? e :
-    (
-        (e.phone_number).includes(searchValue)?
-    e:null
-      ) 
-    ) 
-    )
-    
+    return searchValue == 0 ? e :
+      (toLowerCaseNonAccentVietnamese(e.full_name)
+        .includes(toLowerCaseNonAccentVietnamese(searchValue)) ?
+        e :
+        (toLowerCaseNonAccentVietnamese(e.email)
+          .includes(toLowerCaseNonAccentVietnamese(searchValue))) ?
+          e :
+          (moment(e.date_of_birth).format('DD/MM/YYYY').toString().includes(searchValue) ? e :
+            moment(e.createdAt).format('DD/MM/YYYY').toString().includes(searchValue) ? e :
+              (
+                (e.phone_number).includes(searchValue) ?
+                  e : null
+              )
+          )
+      )
+
   }
 
   // const axiosJWTUpdateAccountStatus = createAxiosJWT(dispatch, user, getAccountSuccess)
-// const [show, setShow] = useState("none")
+  // const [show, setShow] = useState("none")
   const handleChangeAccountStatus = (item) => {
-    // dispatch,id,axiosJWT,accessToken,success,account_id
-    updateAccountStatus(dispatch,item._id,item.is_deleted,axiosJWT,accessToken,getAccountSuccess,user?._id)
+    if (item.role_name == 'admin') {
+      toast.warning("Không thể khóa tài khoản của admin", {
+        position: "top-right",
+      })
+    }
+    else {
+      // dispatch,id,axiosJWT,accessToken,success,account_id
+      updateAccountStatus(dispatch, item._id, item.is_deleted, axiosJWT, accessToken, getAccountSuccess, user?._id)
+    }
+
   }
 
   return (
     <div className='account-management-page container mb-6'>
-              <strong className="is-size-3">Quản lí tài khoản</strong>
+      <strong className="is-size-3">Quản lí tài khoản</strong>
 
       <div className="account-management-overview_div "
         style={{
@@ -134,7 +143,7 @@ function AccountManagementPage() {
                 boxShadow: "0px 0px 10px #ACEFF6"
               }}>
               <thead
-           
+
               >
                 <tr>
                   <th>STT</th>
@@ -151,36 +160,58 @@ function AccountManagementPage() {
                 </tr>
               </thead>
               <tbody>
-                { accounts.
-                filter(item=>handleSearch(item))
-                .map((item) => (
-                  <>
-                   <tr className='mb-2' key={item._id} style={{ background: `${checkColor(item.role_name)}` }}>
-                    <th>{accounts.indexOf(item) + 1}</th>
-                    <td>{item.full_name}</td>
-                    <td>{item.gender}</td>
-                    <td>{moment(item.date_of_birth).format('DD/MM/YYYY')}</td>
-                    <td>{item.email}</td>
-                    <td>{item.phone_number}</td>
-                    <td>{item.role_name}</td>
-                    <td>{moment(item.createdAt).format('DD/MM/YYYY')}</td>
-                    <td>{item.is_deleted?
-                  <button 
-                  className='button is-danger is-small mt-2'
-                   type='button'
-                   onClick={()=>{
-                    handleChangeAccountStatus(item)
-                  }} 
-                   >Bị khóa</button>  :
-                   <button 
-                  className='button is-primary is-small mt-2'
-                   type='button'
-                   onClick={()=>{
-                    handleChangeAccountStatus(item)
-                   }}
-                    >Hoạt động</button>
-                  }</td>
-                    {/* <td  >
+                {accounts.
+                  filter(item => handleSearch(item))
+                  .map((item) => (
+                    <>
+                      <tr className='mb-2' key={item._id} style={{ background: `${checkColor(item.role_name)}` }}>
+                        <th>
+                          <div style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                          {accounts.indexOf(item) + 1}
+                          </div>
+                        </th>
+                        <td>
+                          {item.full_name}
+                        </td>
+                        <td>
+                          <div style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                            {item.gender}
+                          </div>
+                        </td>
+                        <td>{moment(item.date_of_birth).format('DD/MM/YYYY')}</td>
+                        <td>{item.email}</td>
+                        <td>{item.phone_number}</td>
+                        <td>
+                          <div style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                            {item.role_name}
+                          </div>
+                        </td>
+                        <td>
+                          <div style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                            {moment(item.createdAt).format('DD/MM/YYYY')}
+                          </div>
+                        </td>
+                        <td>
+                          <div style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                            {item.is_deleted ?
+                              <button
+                                className='button is-danger is-small '
+                                type='button'
+                                onClick={() => {
+                                  handleChangeAccountStatus(item)
+                                }}
+                              >Bị khóa</button> :
+                              <button
+                                className='button is-primary is-small '
+                                type='button'
+                                onClick={() => {
+                                  handleChangeAccountStatus(item)
+                                }}
+                              >Hoạt động</button>
+                            }
+                          </div>
+                        </td>
+                        {/* <td  >
                       <AiOutlineEdit
                         style={{
                           color: '#008947',
@@ -191,7 +222,7 @@ function AccountManagementPage() {
                           marginTop: ".75rem"
                         }} />
                     </td> */}
-                    {/* <td>
+                        {/* <td>
                       < AiOutlineDelete 
                       // onClick={() => handleDelete(item._id)}
                         style={{
@@ -205,17 +236,17 @@ function AccountManagementPage() {
 
                     </td>
                     */}
-                  </tr>
-                  
-                  </ >
-                 
-                ))}
+                      </tr>
+
+                    </ >
+
+                  ))}
               </tbody>
             </table>
           </div>
         }
       </div>
-     
+
     </div>
   )
 }
